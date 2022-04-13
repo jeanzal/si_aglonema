20672609a847e11f18bd12893a7f7bc24587f4c0
jeanzal <jeanzal61@gmail.com>
2022-04-14 04:10:21 +0700

    Perbaikan Bug Login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,15 +10,51 @@ class Auth extends CI_Controller {
     }
 	public function index()
 	{
-        $data['title'] = 'Login | SISFO AGLONEMA';
-        $this->load->view('templates/auth_header', $data);
-		$this->load->view('auth/login');
-		$this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+        if($this->form_validation->run() == false ){
+            $data['title'] = 'Login | SISFO AGLONEMA';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        }else{
+            $this->_login();
+        }
+        
 	}
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        if($user){
+            if($user['is_active'] == 1){
+                if(password_verify($password, $user['password'])){
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('dashboard');
+                }else{
+                    $this->session->set_flashdata('pesan','<div class="alert alert-danger" role="alert"> Password yang dimasukkan salah! </div>');
+                    redirect('auth');
+                }
+            }else{
+                $this->session->set_flashdata('pesan','<div class="alert alert-warning" role="alert"> Email Blom diaktivasi </div>');
+                redirect('auth');
+            }
+        }else{
+            $this->session->set_flashdata('pesan','<div class="alert alert-danger" role="alert"> Email Tidak Terdaftar </div>');
+            redirect('auth');
+        }
+    }
     public function registrasi()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', 
+            ['is_unique' => 'Email Sudah Terdaftar!']);
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
         $this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[3]|matches[password1]');
         if($this->form_validation->run() == false ){
@@ -28,12 +64,26 @@ class Auth extends CI_Controller {
             $this->load->view('templates/auth_footer');
         }else{
             $data = [
-                'name' => $this->input->post('name'),
-                'email' => $this->input->post('email'),
-                'image' => 'default.jpg',
-                
-            ]
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'gambar' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('pesan','<div class="alert alert-success" role="alert"> Berhasil Membuat Akun </div>');
+            redirect('auth');
         }
         
     }
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+        $this->session->set_flashdata('pesan','<div class="alert alert-success" role="alert"> Berhasil Keluar </div>');
+        redirect('auth');
+    }
+    
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,48 +1,50 @@
+<div class="container">
 
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-    <div class="container">
+        <div class="col-lg-6">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-lg-6">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg ">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">SISTEM INFORMASI AGLONEMA</h1>
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg ">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">SISTEM INFORMASI <br> DIAGNOSA AGLONEMA</h1>
+                                </div>
+                                <?= $this->session->flashdata('pesan'); ?>
+                                <form class="user" method="post" action="<?= base_url('auth'); ?>">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user" id="email"
+                                            name="email" placeholder="Masukkan Email"
+                                            value="<?= set_value('email'); ?>">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>') ?>
                                     </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" 
-                                                id="email" name="email" placeholder="Masukkan Email">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="password" name="password" placeholder="Masukkan Password">
-                                        </div>
-                                        <button type="submit" class="btn btn-success btn-user btn-block">
-                                            Login
-                                        </button>
-                                        
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="<?= base_url('auth/registrasi') ?>">Buat Akun</a>
+                                    <div class=" form-group">
+                                        <input type="password" class="form-control form-control-user" id="password"
+                                            name="password" placeholder="Masukkan Password"
+                                            value="<?= set_value('password'); ?>">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>') ?>
                                     </div>
+                                    <button type=" submit" class="btn btn-success btn-user btn-block">
+                                        Login
+                                    </button>
+
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    <a class="small" href="<?= base_url('auth/registrasi') ?>">Buat Akun</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
 
     </div>
 
+</div>
