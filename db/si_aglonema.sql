8d726931a0910237d77fe2b7748c16f39e18da0c
Jean <jeanzal61@gmail.com>
2022-04-25 11:03:30 +0700

    Update Database

--- db/si_aglonema.sql
+++ db/si_aglonema.sql
@@ -3,55 +3,72 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2022 at 07:29 PM
--- Server version: 10.4.16-MariaDB
--- PHP Version: 7.4.12
-SET
-  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- Waktu pembuatan: 25 Apr 2022 pada 06.02
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET
-  time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-
-/*!40101 SET NAMES utf8mb4 */
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `si_aglonema`
 --
+
 -- --------------------------------------------------------
+
 --
--- Table structure for table `data_tanaman`
+-- Struktur dari tabel `data_tanaman`
 --
+
 CREATE TABLE `data_tanaman` (
   `id_tanaman` int(11) NOT NULL,
   `id_jenis` int(11) DEFAULT NULL,
   `ciri_khas` text DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
+
 --
--- Table structure for table `jenis_aglonema`
+-- Struktur dari tabel `jenis_aglonema`
 --
+
 CREATE TABLE `jenis_aglonema` (
   `id_jenis` int(11) NOT NULL,
   `jenis_aglonema` varchar(50) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
+
 --
--- Table structure for table `user`
+-- Struktur dari tabel `jenis_penyakit`
 --
+
+CREATE TABLE `jenis_penyakit` (
+  `id_penyakit` int(11) NOT NULL,
+  `jenis_penyakit` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jenis_penyakit`
+--
+
+INSERT INTO `jenis_penyakit` (`id_penyakit`, `jenis_penyakit`) VALUES
+(2, 'Mencret Banyak'),
+(3, 'Lambung');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -61,146 +78,114 @@ CREATE TABLE `user` (
   `role_id` int(11) NOT NULL,
   `is_active` int(11) NOT NULL,
   `date_created` date NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
-INSERT INTO
-  `user` (
-    `id`,
-    `name`,
-    `email`,
-    `gambar`,
-    `password`,
-    `role_id`,
-    `is_active`,
-    `date_created`
-  )
-VALUES
-  (
-    1,
-    'admin',
-    'admin@gmail.com',
-    'default.jpg',
-    '$2y$10$4R6Cwcp30mBuTXjW2CJfDu2InCfwC1zCgaIl.U/g7vAS/P7pg7EQi',
-    2,
-    1,
-    '0000-00-00'
-  );
+
+INSERT INTO `user` (`id`, `name`, `email`, `gambar`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(1, 'admin', 'admin@gmail.com', 'default.jpg', '$2y$10$4R6Cwcp30mBuTXjW2CJfDu2InCfwC1zCgaIl.U/g7vAS/P7pg7EQi', 2, 1, '0000-00-00');
 
 -- --------------------------------------------------------
+
 --
--- Table structure for table `user_role`
+-- Struktur dari tabel `user_role`
 --
+
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL,
   `role` varchar(128) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_role`
+-- Dumping data untuk tabel `user_role`
 --
-INSERT INTO
-  `user_role` (`id`, `role`)
-VALUES
-  (1, 'Superadmin'),
-  (2, 'Admin');
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Superadmin'),
+(2, 'Admin');
 
 --
 -- Indexes for dumped tables
 --
---
--- Indexes for table `data_tanaman`
---
-ALTER TABLE
-  `data_tanaman`
-ADD
-  PRIMARY KEY (`id_tanaman`),
-ADD
-  KEY `id_barang` (`id_jenis`);
 
 --
--- Indexes for table `jenis_aglonema`
+-- Indeks untuk tabel `data_tanaman`
 --
-ALTER TABLE
-  `jenis_aglonema`
-ADD
-  PRIMARY KEY (`id_jenis`);
+ALTER TABLE `data_tanaman`
+  ADD PRIMARY KEY (`id_tanaman`),
+  ADD KEY `id_barang` (`id_jenis`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `jenis_aglonema`
 --
-ALTER TABLE
-  `user`
-ADD
-  PRIMARY KEY (`id`);
+ALTER TABLE `jenis_aglonema`
+  ADD PRIMARY KEY (`id_jenis`);
 
 --
--- Indexes for table `user_role`
+-- Indeks untuk tabel `jenis_penyakit`
 --
-ALTER TABLE
-  `user_role`
-ADD
-  PRIMARY KEY (`id`);
+ALTER TABLE `jenis_penyakit`
+  ADD PRIMARY KEY (`id_penyakit`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indeks untuk tabel `user`
 --
---
--- AUTO_INCREMENT for table `data_tanaman`
---
-ALTER TABLE
-  `data_tanaman`
-MODIFY
-  `id_tanaman` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 5;
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for table `jenis_aglonema`
+-- Indeks untuk tabel `user_role`
 --
-ALTER TABLE
-  `jenis_aglonema`
-MODIFY
-  `id_jenis` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 16;
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
-ALTER TABLE
-  `user`
-MODIFY
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 2;
 
 --
--- AUTO_INCREMENT for table `user_role`
+-- AUTO_INCREMENT untuk tabel `data_tanaman`
 --
-ALTER TABLE
-  `user_role`
-MODIFY
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 3;
+ALTER TABLE `data_tanaman`
+  MODIFY `id_tanaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT untuk tabel `jenis_aglonema`
 --
---
--- Constraints for table `data_tanaman`
---
-ALTER TABLE
-  `data_tanaman`
-ADD
-  CONSTRAINT `id_barang` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_aglonema` (`id_jenis`);
+ALTER TABLE `jenis_aglonema`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
+--
+-- AUTO_INCREMENT untuk tabel `jenis_penyakit`
+--
+ALTER TABLE `jenis_penyakit`
+  MODIFY `id_penyakit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `data_tanaman`
+--
+ALTER TABLE `data_tanaman`
+  ADD CONSTRAINT `id_barang` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_aglonema` (`id_jenis`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
